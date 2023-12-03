8390bb8adae3520796315c285623341cd1e4b584
Trương Minh Phung <minhphung485@gmail.com>
2023-12-03 13:32:26 +0700

    up

--- app/Helpers/Common.php
+++ app/Helpers/Common.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Pagination\LengthAwarePaginator;
 
 if (!function_exists('includeAdapter')) {
@@ -30,5 +31,52 @@ if (!function_exists('paginateCustom')) {
     $dataPaginated->setPath(request()->url());
 
     return $dataPaginated;
+  }
+}
+
+if (!function_exists('caculateTimes')) {
+  function caculateTimes(
+    $startDate,
+    $endDate,
+    $configs,
+    $break = 0,
+    $flagSubEveryConfig = true
+  ) {
+    $datesInRange = [];
+    $startDate = Carbon::parse($startDate);
+    $endDate = Carbon::parse($endDate);
+
+
+    while ($startDate->lte($endDate)) {
+      $datesInRange[] = $startDate->toDateString();
+      $startDate->addDay();
+    }
+    $totalMinutes = 0;
+    foreach ($datesInRange as $key => $date) {
+      foreach ($configs as $key2 => $config) {
+        $configTimeStart = Carbon::parse($config['start_time']);
+        $configTimeEnd = Carbon::parse($config['end_time']);
+        if ($key == 0) {
+          if ($configTimeStart->format('H:i:s') >= $startDate->format('H:i:s')) {
+            $totalMinutes += $configTimeEnd->diffInMinutes($configTimeStart);
+          }
+        } elseif ($key == count($datesInRange)) {
+          if ($configTimeEnd->format('H:i:s') <= $endDate->format('H:i:s')) {
+            $totalMinutes += $configTimeEnd->diffInMinutes($configTimeStart);
+          }
+        } else {
+          $totalMinutes += $configTimeEnd->diffInMinutes($configTimeStart);
+        }
+
+        if ($flagSubEveryConfig) {
+          $totalMinutes -= $break * 60;
+        }
+      }
+    }
+    $totalHours = ($totalMinutes / 60);
+    if (!$flagSubEveryConfig) {
+      $totalHours = $totalHours - $break;
+    }
+    return $totalHours;
   }
 }
